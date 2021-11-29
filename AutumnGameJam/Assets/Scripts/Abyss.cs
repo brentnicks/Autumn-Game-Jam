2d1d17438c451a16804e813bfb4c2202cbaf7d15
brentnicks <brent_nicks@msn.com>
2021-11-28 21:40:58 -0800

    1

---
+++ AutumnGameJam/Assets/Scripts/Abyss.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Abyss : MonoBehaviour
+{
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.name == "Player")
+        {
+            Time.timeScale = 0;
+            Debug.LogError("Game Over!");
+        }
+    }
+}
--- AutumnGameJam/Assets/Scripts/NextStage.cs
+++ AutumnGameJam/Assets/Scripts/NextStage.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class NextStage : MonoBehaviour
 {
-    public GameObject fallingGameSpawner;
     public GameObject player;
+    public GameObject gm;
     public GameObject[] Stages;
+    public Camera camera;
+    [HideInInspector] public int stageNumber = 1;
+    public GameObject stageNumberText;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.gameObject.Equals(player))
         {
-            fallingGameSpawner.transform.position = new Vector3(0, gameObject.transform.position.y + 50, 0);
-            Instantiate(Stages[Random.Range(0,Stages.Length-1)], fallingGameSpawner.transform.position, Quaternion.identity);
-            gameObject.SetActive(false);
+            gm.transform.position = new Vector3(0, gameObject.transform.position.y + 20.5f, 0);
+            Instantiate(Stages[Random.Range(0,Stages.Length-1)], gm.transform.position, Quaternion.identity);
+            camera.transform.position = new Vector3(0, camera.transform.position.y + 20.5f, -20);
+            ++stageNumber;
+            stageNumberText.GetComponent<Text>().text = stageNumber.ToString();
+
         }
     }
 }
